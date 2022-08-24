4b343e90dfbe24f999401e300fb8e1be332e9f80
Garvish <garvish.p@theintellify.com>
2022-08-24 11:27:16 +0530

    Code push from macbook

---
+++ In-House---Car-Battle-Project-main/Assets/Scripts/InGameUiManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameUiManager : MonoBehaviour
+{
+    [Header ("Canvas")]
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject inputPanel;
+    [SerializeField] GameObject nullPanel;
+
+    private GameObject currentPanel;
+
+    [SerializeField] string HomeScene;
+    [SerializeField] string CurrentScene;
+
+    private void Start()
+    {
+        // close panel
+        pauseMenu.SetActive(false);
+        nullPanel.SetActive(false);
+
+        // change to input panel
+        ChangePanel(inputPanel);
+    }
+
+    void ChangePanel (GameObject activePanel)
+    {
+        if (currentPanel != null)
+        {
+            currentPanel.SetActive(false);
+            Debug.Log($"Panel available");
+        }
+        else Debug.Log($"No panel available");
+
+        activePanel.SetActive(true);
+        currentPanel = activePanel;
+    }
+
+    void ChangeTime(int timeValue = 0) => Time.timeScale = timeValue;
+
+    void ChangeScene(string sceneName) => SceneManager.LoadScene(sceneName);
+
+    public void B_PauseMenu()
+    {
+        ChangeTime(0);
+        ChangePanel(pauseMenu);
+    }
+
+    public void B_Resume ()
+    {
+        ChangeTime(1);
+        ChangePanel(inputPanel);
+    }
+
+    public void B_Home ()
+    {
+        ChangeTime(1);
+        ChangeScene(HomeScene);
+    }
+
+    public void B_Restart ()
+    {
+        ChangeTime(1);
+        ChangeScene(CurrentScene);
+    }
+}
---
+++ In-House---Car-Battle-Project-main/Assets/Scripts/MainMenuUIManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    [Header ("Main Menu")]
+    [SerializeField] GameObject settingPanel;
+    [SerializeField] GameObject achivementPanel;
+    [SerializeField] GameObject infoCanvas;
+    [Space]
+    [SerializeField] GameObject currentCanvas;
+
+
+    private void Start()
+    {
+        settingPanel.SetActive(false);
+        infoCanvas.SetActive(false);
+        achivementPanel.SetActive(false);
+    }
+
+    public void _ChangeCanvas (GameObject activeCanvas)
+    {
+        if (currentCanvas != null)
+        {
+            currentCanvas.SetActive(false);
+        }
+
+        activeCanvas.SetActive(true);
+        currentCanvas = activeCanvas;
+    }
+
+    public void B_BackButton ()
+    {
+        if (currentCanvas != null)
+            currentCanvas.SetActive(false);
+    }
+
+    [Space]
+    [Header("Modes")]
+    [SerializeField] GameObject[] VehicleCanvas;
+    [SerializeField] GameObject[] weaponCanvas;
+
+    public void B_Weapon ()
+    {
+        for (int i = 0; i < VehicleCanvas.Length; i++)
+        {
+            VehicleCanvas[i].SetActive(false);
+        }
+
+        for (int i = 0; i < weaponCanvas.Length; i++)
+        {
+            weaponCanvas[i].SetActive(true);
+        }
+    }
+
+    public void B_Vehicle ()
+    {
+        for (int i = 0; i < weaponCanvas.Length; i++)
+        {
+            weaponCanvas[i].SetActive(false);
+        }
+
+        for (int i = 0; i < VehicleCanvas.Length; i++)
+        {
+            VehicleCanvas[i].SetActive(true);
+        }
+
+    }
+}
