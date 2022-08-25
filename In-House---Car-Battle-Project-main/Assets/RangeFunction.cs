a318518172e2c7080be99d2e23205bf7f74c6ec8
Garvish <garvish.p@theintellify.com>
2022-08-25 18:28:08 +0530

    Shooting Function

---
+++ In-House---Car-Battle-Project-main/Assets/RangeFunction.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class RangeFunction : MonoBehaviour
+{
+    public static Action<Transform> TargetLocked;
+    public static Action TragetLost;
+
+    [SerializeField] string AiCarString;
+
+
+    private void OnTriggerEnter(Collider Info)
+    {
+        Debug.Log("Target locked");
+        if (Info.CompareTag(AiCarString))
+        {
+            TargetLocked?.Invoke(Info.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider Info)
+    {
+        if (Info.CompareTag(AiCarString))
+            TragetLost?.Invoke();
+    }
+}
---
+++ In-House---Car-Battle-Project-main/Assets/Scripts/ShootingSystem.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class ShootingSystem : MonoBehaviour
+{
+    [Header ("Components")]
+    [SerializeField] Transform target;
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform radiusCollider;
+
+    [Space]
+    [Header("Components info")]
+    [Range(1f, 20f)]
+    [SerializeField] float radius = 1;
+    [SerializeField] Vector3 radiusVector;
+    [Space]
+    [SerializeField] bool changeRadius = false;
+
+    private void OnEnable()
+    {
+        RangeFunction.TargetLocked += OnTargerLocked;
+        RangeFunction.TragetLost += OnTargerLost;
+    }
+
+    private void OnDisable()
+    {
+        RangeFunction.TargetLocked -= OnTargerLocked;
+        RangeFunction.TragetLost -= OnTargerLost;
+    }
+
+    private void Start()
+    {
+        radiusVector.x = radius;
+        radiusVector.y = radius;
+        radiusVector.z = radius;
+        radiusCollider.localScale = radiusVector;
+    }
+
+    private void Update()
+    {
+        if (changeRadius is true)
+            ChangeRadius(radius);
+    }
+
+    private void ChangeRadius (float L_radius)
+    {
+        radiusVector.x = L_radius;
+        radiusVector.y = L_radius;
+        radiusVector.z = L_radius;
+        radiusCollider.localScale = radiusVector;
+    }
+
+    void OnTargerLocked(Transform l_targert) => target = l_targert;
+
+    void OnTargerLost() => target = null;
+}
